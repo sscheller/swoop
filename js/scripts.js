ea0e4e613e65b8c0e7937d559c86aa497fe5f4cf
Sonja Scheller <sscheller98@live.com>
2019-06-05 11:22:03 -0700

    Updated JavaScript
    Added Choclat Plugin and options

--- js/scripts.js
+++ js/scripts.js
@@ -3,3 +3,11 @@ console.log('OK');
 // Page Headings
 // Comments
 $('h1').lettering();
+
+//Portfolio
+
+$('.portfolio').Chocolat({
+    enableZoom : false,
+    seperator2 :  '- -',
+    fullScreen: true,
+});
